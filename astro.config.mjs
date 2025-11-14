30605a169ee18acb2b8a349ac173ede2d766539b
Moritz Enderle <contact@moritzenderle.com>
2025-11-14 11:12:20 +0100

    Fixes deployment issue with build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
- 
+
+// ADD THIS LINE:
+import node from '@astrojs/node';
+
 export default defineConfig({
   output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  server: { port: 3000, host: true },
   vite: {
     plugins: [tailwindcss()]
   }
